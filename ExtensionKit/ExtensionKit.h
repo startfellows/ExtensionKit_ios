243cc1c75bc2b58faf7234c6ced83b40eb3304ef
Kirill Kovalev <kirilkovalev@yandex.ru>
2021-12-18 15:54:28 +0300

    Initial Commit

---
+++ ExtensionKit/ExtensionKit.h
@@ -0,0 +1,18 @@
+//
+//  ExtensionKit.h
+//  ExtensionKit
+//
+//  Created by Кирилл on 18.12.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ExtensionKit.
+FOUNDATION_EXPORT double ExtensionKitVersionNumber;
+
+//! Project version string for ExtensionKit.
+FOUNDATION_EXPORT const unsigned char ExtensionKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExtensionKit/PublicHeader.h>
+
+
