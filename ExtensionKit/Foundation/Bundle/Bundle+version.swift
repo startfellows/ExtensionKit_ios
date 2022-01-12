02992a57fdbd172cc116ba7a28b8f919c7265d2f
Kirill Kovalev <kirilkovalev@yandex.ru>
2022-01-12 12:32:34 +0300

    * | Bundle | changed release/build numbers from String? to String!

--- ExtensionKit/Foundation/Bundle/Bundle+version.swift
+++ ExtensionKit/Foundation/Bundle/Bundle+version.swift
@@ -6,6 +6,6 @@
 import Foundation
 
 public extension Bundle {
-    var releaseVersionNumber: String? { infoDictionary?["CFBundleShortVersionString"] as? String }
-    var buildVersionNumber: String? { infoDictionary?["CFBundleVersion"] as? String }
+    var releaseVersionNumber: String! { infoDictionary?["CFBundleShortVersionString"] as? String }
+    var buildVersionNumber: String! { infoDictionary?["CFBundleVersion"] as? String }
 }
