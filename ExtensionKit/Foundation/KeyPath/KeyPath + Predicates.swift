f5b201c1a64a4163cb541a3bd8910bed9818d771
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-02-18 19:49:49 +0300

    Update KeyPath + Predicates.swift

--- ExtensionKit/Foundation/KeyPath/KeyPath + Predicates.swift
+++ ExtensionKit/Foundation/KeyPath/KeyPath + Predicates.swift
@@ -5,63 +5,100 @@
 
 import Foundation
 
-public typealias Predicate<T> = Equation<T,Bool>
+public typealias Predicate<T> = Equation<T, Bool>
 
-// MARK: - Combine keypaths with keypaths
-public prefix func !<T>(keyPath: KeyPath<T, Bool>) -> Predicate<T> {
-    return { !$0[keyPath: keyPath] }
+public extension KeyPath where Value: Equatable {
+    // MARK: - Combine keypaths with keypath
+    static func ==(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
+        return { $0[keyPath: lhs] == $0[keyPath: rhs] }
+    }
+    
+    static func !=(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
+        return { $0[keyPath: lhs] != $0[keyPath: rhs] }
+    }
+    
+    static func ==(lhs: KeyPath, rhs: KeyPath<Root, Value?>) -> Predicate<Root> {
+        return { $0[keyPath: lhs] == $0[keyPath: rhs] }
+    }
+    
+    static func !=(lhs: KeyPath, rhs: KeyPath<Root, Value?>) -> Predicate<Root> {
+        return { $0[keyPath: lhs] != $0[keyPath: rhs] }
+    }
+    
+    // MARK: - Combine keypaths with values
+    static func ==(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
+        return { $0[keyPath: lhs] == rhs }
+    }
+    
+    static func !=(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
+        return { $0[keyPath: lhs] != rhs }
+    }
+    
+    // MARK: - Combine keypaths with optionals
+    static func ==(lhs: KeyPath, rhs: Value?) -> Predicate<Root> {
+        return { $0[keyPath: lhs] == rhs }
+    }
+    
+    static func !=(lhs: KeyPath, rhs: Value?) -> Predicate<Root> {
+        return { $0[keyPath: lhs] != rhs }
+    }
+    
+    static func ==(lhs: KeyPath<Root, Value?>, rhs: Value) -> Predicate<Root> {
+        return { $0[keyPath: lhs] == rhs }
+    }
+
+    static func !=(lhs: KeyPath<Root, Value?>, rhs: Value) -> Predicate<Root> {
+        return { $0[keyPath: lhs] != rhs }
+    }
+}
+    
+public extension KeyPath where Value: AnyObject {
+    // MARK: - Combine keypaths with objects
+    static func ===(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
+        return { $0[keyPath: lhs] === rhs }
+    }
+    
+    static func !==(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
+        return { $0[keyPath: lhs] !== rhs }
+    }
+    
 }
 
-public func ==<T, V: Equatable>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Predicate<T> {
-    return { $0[keyPath: lhs] == $0[keyPath: rhs] }
+public extension KeyPath where Value: Comparable {
+    // MARK: - Combine keypaths with keypath
+    static func >(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
+        return { $0[keyPath: lhs] > $0[keyPath: rhs] }
+    }
+    
+    static func <(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
+        return { $0[keyPath: lhs] < $0[keyPath: rhs] }
+    }
+    
+    // MARK: - Combine keypaths with values
+    static func ><T, V: Comparable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
+        return { $0[keyPath: lhs] > rhs }
+    }
+    
+    static func <<T, V: Comparable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
+        return { $0[keyPath: lhs] < rhs }
+    }
 }
-
-public func !=<T, V: Equatable>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Predicate<T> {
-    return { $0[keyPath: lhs] != $0[keyPath: rhs] }
+    
+public extension KeyPath where Value == Bool {
+    // MARK: - Combine keypaths with keypaths
+    static prefix func !(keyPath: KeyPath<Root, Value>) -> Predicate<Root> {
+        return { !$0[keyPath: keyPath] }
+    }
+    
+    // MARK: - Combine Predicates with keypaths
+    static func &&(lhs: @escaping Predicate<Root>, rhs: KeyPath<Root, Value>) -> Predicate<Root> {
+        return { lhs($0) && $0[keyPath: rhs] }
+    }
+    
+    static func ||(lhs: @escaping Predicate<Root>, rhs: KeyPath<Root, Value>) -> Predicate<Root> {
+        return { lhs($0) || $0[keyPath: rhs] }
+    }
 }
-
-public func ><T, V: Comparable>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Predicate<T> {
-    return { $0[keyPath: lhs] > $0[keyPath: rhs] }
-}
-
-public func <<T, V: Comparable>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Predicate<T> {
-    return { $0[keyPath: lhs] < $0[keyPath: rhs] }
-}
-
-// MARK: - Combine keypaths with values
-public func ==<T, V: Equatable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
-    return { $0[keyPath: lhs] == rhs }
-}
-
-public func !=<T, V: Equatable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
-    return { $0[keyPath: lhs] != rhs }
-}
-
-public func ===<T, V: AnyObject>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
-    return { $0[keyPath: lhs] === rhs }
-}
-
-public func !==<T, V: AnyObject>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
-    return { $0[keyPath: lhs] !== rhs }
-}
-
-public func ><T, V: Comparable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
-    return { $0[keyPath: lhs] > rhs }
-}
-
-public func <<T, V: Comparable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
-    return { $0[keyPath: lhs] < rhs }
-}
-
-// MARK: - Combine Predicates with keypaths
-public func &&<T>(lhs: @escaping Predicate<T>, rhs: KeyPath<T, Bool>) -> Predicate<T> {
-    return { lhs($0) && $0[keyPath: rhs] }
-}
-
-public func ||<T>(lhs: @escaping Predicate<T>, rhs: KeyPath<T, Bool>) -> Predicate<T> {
-    return { lhs($0) || $0[keyPath: rhs] }
-}
-
 
 // MARK: - Combine Predicates with Predicates
 public prefix func !<T>(predicate: @escaping Predicate<T>) -> Predicate<T> {
@@ -75,4 +112,3 @@ public func &&<T>(lhs: @escaping Predicate<T>, rhs: @escaping Predicate<T>) -> P
 public func ||<T>(lhs: @escaping Predicate<T>, rhs: @escaping Predicate<T>) -> Predicate<T> {
     return { lhs($0) || rhs($0) }
 }
-
