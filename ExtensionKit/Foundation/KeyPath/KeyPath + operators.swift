2c7b29a323325c0d0a58e27c159f0b1c80d9a57d
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-02-18 19:50:20 +0300

    Update KeyPath + operators.swift

--- ExtensionKit/Foundation/KeyPath/KeyPath + operators.swift
+++ ExtensionKit/Foundation/KeyPath/KeyPath + operators.swift
@@ -7,58 +7,100 @@ import Foundation
 
 public typealias Equation<T, V> = (T) -> V
 
-// MARK: - Combine keypaths with keypaths
-public func +<T, V: Numeric>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Equation<T, V> {
-    return { $0[keyPath: lhs] + $0[keyPath: rhs] }
+public extension KeyPath where Value: Numeric {
+    // MARK: - Combine keypaths with keypaths
+    static func +(lhs: KeyPath, rhs: KeyPath) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] + $0[keyPath: rhs] }
+    }
+
+    static func -(lhs: KeyPath, rhs: KeyPath) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] - $0[keyPath: rhs] }
+    }
+
+    static func *(lhs: KeyPath, rhs: KeyPath) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] * $0[keyPath: rhs] }
+    }
+    
+    // MARK: - Combine keypaths with values
+    static func +(lhs: KeyPath, rhs: Value) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] + rhs }
+    }
+
+    static func -(lhs: KeyPath, rhs: Value) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] - rhs }
+    }
+
+    static func *(lhs: KeyPath, rhs: Value) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] * rhs }
+    }
+}
+public extension KeyPath where Value: FloatingPoint {
+    
+    // MARK: - Combine keypaths with keypaths
+    static func /(lhs: KeyPath, rhs: KeyPath) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] / $0[keyPath: rhs] }
+    }
+    
+    // MARK: - Combine keypaths with values
+    static func /(lhs: KeyPath, rhs: Value) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] / rhs }
+    }
 }
 
-public func -<T, V: Numeric>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Equation<T, V> {
-    return { $0[keyPath: lhs] - $0[keyPath: rhs] }
+// MARK: - Combine Equation with Equation
+public func +<T, V: Numeric>(lhs: @escaping Equation<T,V>, rhs: @escaping Equation<T,V>) -> Equation<T,V> {
+    return { lhs($0) + rhs($0) }
 }
-
-public func *<T, V: Numeric>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Equation<T, V> {
-    return { $0[keyPath: lhs] * $0[keyPath: rhs] }
+public func -<T, V: Numeric>(lhs: @escaping Equation<T,V>, rhs: @escaping Equation<T,V>) -> Equation<T,V> {
+    return { lhs($0) - rhs($0) }
 }
-
-public func /<T, V: FloatingPoint>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Equation<T, V> {
-    return { $0[keyPath: lhs] / $0[keyPath: rhs] }
+public func *<T, V: Numeric>(lhs: @escaping Equation<T,V>, rhs: @escaping Equation<T,V>) -> Equation<T,V> {
+    return { lhs($0) * rhs($0) }
 }
-
-// MARK: - Combine keypaths with values
-public func +<T, V: Numeric>(lhs: KeyPath<T, V>, rhs: V) -> Equation<T, V> {
-    return { $0[keyPath: lhs] + rhs }
+public func /<T, V: FloatingPoint>(lhs: @escaping Equation<T,V>, rhs: @escaping Equation<T,V>) -> Equation<T,V> {
+    return { lhs($0) / rhs($0) }
 }
+// MARK: - Combine Equation with Equation
 
-public func -<T, V: Numeric>(lhs: KeyPath<T, V>, rhs: V) -> Equation<T, V> {
-    return { $0[keyPath: lhs] - rhs }
+public func +<T, V: Numeric>(lhs: @escaping Equation<T,V>, rhs: V) -> Equation<T,V> {
+    return { lhs($0) + rhs }
 }
-
-public func *<T, V: Numeric>(lhs: KeyPath<T, V>, rhs: V) -> Equation<T, V> {
-    return { $0[keyPath: lhs] * rhs }
+public func -<T, V: Numeric>(lhs: @escaping Equation<T,V>, rhs: V) -> Equation<T,V> {
+    return { lhs($0) - rhs }
 }
-
-public func /<T, V: FloatingPoint>(lhs: KeyPath<T, V>, rhs: V) -> Equation<T, V> {
-    return { $0[keyPath: lhs] / rhs }
+public func *<T, V: Numeric>(lhs: @escaping Equation<T,V>, rhs: V) -> Equation<T,V> {
+    return { lhs($0) * rhs }
+}
+public func /<T, V: FloatingPoint>(lhs: @escaping Equation<T,V>, rhs: V) -> Equation<T,V> {
+    return { lhs($0) / rhs }
 }
 
 // MARK: - Collections
-public func +<T, V: RangeReplaceableCollection>(lhs: KeyPath<T, V>, rhs: KeyPath<T, V>) -> Equation<T, V> {
-    return { $0[keyPath: lhs] + $0[keyPath: rhs] }
+public extension KeyPath where Value: RangeReplaceableCollection {
+    static func +(lhs: KeyPath, rhs: KeyPath) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] + $0[keyPath: rhs] }
+    }
+    
+    static func +(lhs: KeyPath, rhs: Value) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] + rhs }
+    }
 }
 
 // MARK: - Optionals
-public func ??<T, V>(lhs: KeyPath<T, V?>, rhs: V) -> Equation<T, V> {
-    return { $0[keyPath: lhs] ?? rhs }
-}
-
-public func ??<T, V>(lhs: KeyPath<T, V?>, rhs: V?) -> Equation<T, V?> {
-    return { $0[keyPath: lhs] ?? rhs }
-}
-
-public func ??<T, V>(lhs: KeyPath<T, V?>, rhs: KeyPath<T, V>) -> Equation<T, V> {
-    return { $0[keyPath: lhs] ?? $0[keyPath: rhs] }
-}
-
-public func ??<T, V>(lhs: KeyPath<T, V?>, rhs: KeyPath<T, V?>) -> Equation<T, V?> {
-    return { $0[keyPath: lhs] ?? $0[keyPath: rhs] }
+public extension KeyPath {
+    static func ??(lhs: KeyPath<Root, Value?>, rhs: Value) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] ?? rhs }
+    }
+    
+    static func ??(lhs: KeyPath<Root, Value?>, rhs: Value?) -> Equation<Root, Value?> {
+        return { $0[keyPath: lhs] ?? rhs }
+    }
+    
+    static func ??(lhs: KeyPath<Root, Value?>, rhs: KeyPath<Root, Value>) -> Equation<Root, Value> {
+        return { $0[keyPath: lhs] ?? $0[keyPath: rhs] }
+    }
+    
+    static func ??(lhs: KeyPath<Root, Value?>, rhs: KeyPath<Root, Value?>) -> Equation<Root, Value?> {
+        return { $0[keyPath: lhs] ?? $0[keyPath: rhs] }
+    }
 }
