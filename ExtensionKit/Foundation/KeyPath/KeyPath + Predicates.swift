957af6bdb4ffc8e136147540bd10ca5a7a103ea5
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-04-25 19:58:12 +0300

    @inlinable

--- ExtensionKit/Foundation/KeyPath/KeyPath + Predicates.swift
+++ ExtensionKit/Foundation/KeyPath/KeyPath + Predicates.swift
@@ -9,44 +9,54 @@ public typealias Predicate<T> = Equation<T, Bool>
 
 public extension KeyPath where Value: Equatable {
     // MARK: - Combine keypaths with keypath
+    @inlinable @inline(__always)
     public static func ==(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
         return { $0[keyPath: lhs] == $0[keyPath: rhs] }
     }
     
+    @inlinable @inline(__always)
     public static func !=(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
         return { $0[keyPath: lhs] != $0[keyPath: rhs] }
     }
     
+    @inlinable @inline(__always)
     public static func ==(lhs: KeyPath, rhs: KeyPath<Root, Value?>) -> Predicate<Root> {
         return { $0[keyPath: lhs] == $0[keyPath: rhs] }
     }
     
+    @inlinable @inline(__always)
     public static func !=(lhs: KeyPath, rhs: KeyPath<Root, Value?>) -> Predicate<Root> {
         return { $0[keyPath: lhs] != $0[keyPath: rhs] }
     }
     
     // MARK: - Combine keypaths with values
+    @inlinable @inline(__always)
     public static func ==(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
         return { $0[keyPath: lhs] == rhs }
     }
     
+    @inlinable @inline(__always)
     public static func !=(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
         return { $0[keyPath: lhs] != rhs }
     }
     
     // MARK: - Combine keypaths with optionals
+    @inlinable @inline(__always)
     public static func ==(lhs: KeyPath, rhs: Value?) -> Predicate<Root> {
         return { $0[keyPath: lhs] == rhs }
     }
     
+    @inlinable @inline(__always)
     public static func !=(lhs: KeyPath, rhs: Value?) -> Predicate<Root> {
         return { $0[keyPath: lhs] != rhs }
     }
     
+    @inlinable @inline(__always)
     public static func ==(lhs: KeyPath<Root, Value?>, rhs: Value) -> Predicate<Root> {
         return { $0[keyPath: lhs] == rhs }
     }
-
+    
+    @inlinable @inline(__always)
     public static func !=(lhs: KeyPath<Root, Value?>, rhs: Value) -> Predicate<Root> {
         return { $0[keyPath: lhs] != rhs }
     }
@@ -55,21 +65,25 @@ public extension KeyPath where Value: Equatable {
 // MARK: - Combine keypaths with nil
 public extension KeyPath {
     
+    @inlinable @inline(__always)
     public static func ==(lhs: KeyPath<Root, Value?>, rhs: _OptionalNilComparisonType) -> Predicate<Root> {
         return { $0[keyPath: lhs] == rhs }
     }
     
+    @inlinable @inline(__always)
     public static func !=(lhs: KeyPath<Root, Value?>, rhs: _OptionalNilComparisonType) -> Predicate<Root> {
         return { $0[keyPath: lhs] != rhs }
     }
 }
-    
+
 public extension KeyPath where Value: AnyObject {
     // MARK: - Combine keypaths with objects
+    @inlinable @inline(__always)
     public static func ===(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
         return { $0[keyPath: lhs] === rhs }
     }
     
+    @inlinable @inline(__always)
     public static func !==(lhs: KeyPath, rhs: Value) -> Predicate<Root> {
         return { $0[keyPath: lhs] !== rhs }
     }
@@ -78,50 +92,60 @@ public extension KeyPath where Value: AnyObject {
 
 public extension KeyPath where Value: Comparable {
     // MARK: - Combine keypaths with keypath
+    @inlinable @inline(__always)
     public static func >(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
         return { $0[keyPath: lhs] > $0[keyPath: rhs] }
     }
     
+    @inlinable @inline(__always)
     public static func <(lhs: KeyPath, rhs: KeyPath) -> Predicate<Root> {
         return { $0[keyPath: lhs] < $0[keyPath: rhs] }
     }
     
     // MARK: - Combine keypaths with values
     
+    @inlinable @inline(__always)
     public static func ><T, V: Comparable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
         return { $0[keyPath: lhs] > rhs }
     }
     
+    @inlinable @inline(__always)
     public static func <<T, V: Comparable>(lhs: KeyPath<T, V>, rhs: V) -> Predicate<T> {
         return { $0[keyPath: lhs] < rhs }
     }
 }
-    
+
 public extension KeyPath where Value == Bool {
     // MARK: - Combine keypaths with keypaths
+    @inlinable @inline(__always)
     public static prefix func !(keyPath: KeyPath<Root, Value>) -> Predicate<Root> {
         return { !$0[keyPath: keyPath] }
     }
     
     // MARK: - Combine Predicates with keypaths
+    @inlinable @inline(__always)
     public static func &&(lhs: @escaping Predicate<Root>, rhs: KeyPath<Root, Value>) -> Predicate<Root> {
         return { lhs($0) && $0[keyPath: rhs] }
     }
     
+    @inlinable @inline(__always)
     public static func ||(lhs: @escaping Predicate<Root>, rhs: KeyPath<Root, Value>) -> Predicate<Root> {
         return { lhs($0) || $0[keyPath: rhs] }
     }
 }
 
 // MARK: - Combine Predicates with Predicates
+@inlinable @inline(__always)
 public prefix func !<T>(predicate: @escaping Predicate<T>) -> Predicate<T> {
     return { !predicate($0) }
 }
 
+@inlinable @inline(__always)
 public func &&<T>(lhs: @escaping Predicate<T>, rhs: @escaping Predicate<T>) -> Predicate<T> {
     return { lhs($0) && rhs($0) }
 }
 
+@inlinable @inline(__always)
 public func ||<T>(lhs: @escaping Predicate<T>, rhs: @escaping Predicate<T>) -> Predicate<T> {
     return { lhs($0) || rhs($0) }
 }
