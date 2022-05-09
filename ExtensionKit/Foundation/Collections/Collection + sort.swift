b2e48382808b71850fa66ac17de15f90c7022f3c
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-05-09 22:47:46 +0300

    Update Collection + sort.swift

--- ExtensionKit/Foundation/Collections/Collection + sort.swift
+++ ExtensionKit/Foundation/Collections/Collection + sort.swift
@@ -6,15 +6,24 @@
 import Foundation
 
 public extension Collection {
+    
+    @available(*, deprecated)
     func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>) -> [Element] {
-        return sorted(by: {
-            $0[keyPath: keyPath] > $1[keyPath: keyPath]
-        })
+        return sorted(by: keyPath, >)
     }
     
-    func sorted<T: Comparable>(by keyPath: KeyPath<Element, T>, _ comparator: (T, T) -> Bool) -> [Element] {
+    func sorted<T: Comparable>(asc keyPath: KeyPath<Element, T>) -> [Element] {
+        return sorted(by: keyPath, >)
+    }
+    
+    func sorted<T: Comparable>(desc keyPath: KeyPath<Element, T>) -> [Element] {
+        return sorted(by: keyPath, <)
+    }
+    
+    func sorted<T>(by keyPath: KeyPath<Element, T>, _ comparator: (T, T) -> Bool) -> [Element] {
         return sorted(by: {
             comparator($0[keyPath: keyPath], $1[keyPath: keyPath])
         })
     }
 }
+
