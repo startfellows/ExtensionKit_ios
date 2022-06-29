ecc0dbf540fb02aa6f426496b070d2fa7037b4eb
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-06-29 14:13:18 +0300

    Update Collection+subscript.swift

--- ExtensionKit/Foundation/Collections/Collection+subscript.swift
+++ ExtensionKit/Foundation/Collections/Collection+subscript.swift
@@ -18,3 +18,17 @@ public extension Collection {
         at(index)
     }
 }
+
+public extension Collection {
+    func at<T>(_ index: Index) -> T? where T? == Element {
+        guard index >= self.startIndex,
+              index < self.endIndex
+        else { return nil }
+
+        return self[index]
+    }
+    
+    subscript<T>(safe index: Index) -> T? where T? == Element  {
+        at(index)
+    }
+}
