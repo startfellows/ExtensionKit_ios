3804fb1711eed6bd53551ac3343eedd9f69aea52
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-08-09 11:29:19 +0000

    Update UIView + layout.swift

--- ExtensionKit/UIKit/UIVIew/UIView + layout.swift
+++ ExtensionKit/UIKit/UIVIew/UIView + layout.swift
@@ -7,11 +7,24 @@ import UIKit
 
 public extension UIView {
     
+    @available(*, deprecated)
     func systemLayoutSizeFittingWidth(_ width: CGFloat) -> CGSize {
+        systemLayoutSizeFitting(width: width)
+    }
+    
+    func systemLayoutSizeFitting(width: CGFloat) -> CGSize {
         let fittingSize = CGSize(width: width, height: 0)
         let newSize = self.systemLayoutSizeFitting(fittingSize,
                                                    withHorizontalFittingPriority: .required,
                                                    verticalFittingPriority: .fittingSizeLevel)
+        return newSize
+    }
+    
+    func systemLayoutSizeFitting(height: CGFloat) -> CGSize {
+        let fittingSize = CGSize(width: 0, height: height)
+        let newSize = self.systemLayoutSizeFitting(fittingSize,
+                                                   withHorizontalFittingPriority: .fittingSizeLevel,
+                                                   verticalFittingPriority: .required)
         return newSize
     }
     
