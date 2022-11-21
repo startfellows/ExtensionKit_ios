e55f0ec60839bd0988bcef9aed1e67af9f4d887d
Kirill <61139350+kirill-kovalev@users.noreply.github.com>
2022-11-21 21:12:04 +0100

    Update Collection + max.swift

--- ExtensionKit/Foundation/Collections/Collection + max.swift
+++ ExtensionKit/Foundation/Collections/Collection + max.swift
@@ -2,18 +2,18 @@
 //  Collection + sort.swift
 //  ExtensionKit
 //
-
-import Foundation
-
-public extension Collection {
-
-    func max<T: Comparable>(by keyPath: KeyPath<Element, T>, _ comparator: (T, T) -> Bool) -> Element? {
-        max(by: {
-            comparator($0[keyPath: keyPath], $1[keyPath: keyPath])
-        })
-    }
-    
-    func max<T: Comparable>(by keyPath: KeyPath<Element, T>) -> Element? {
-        max(by: keyPath, >)
-    }
-}
+//
+//import Foundation
+//
+//public extension Collection {
+//
+//    func max<T: Comparable>(by keyPath: KeyPath<Element, T>, _ comparator: (T, T) -> Bool) -> Element? {
+//        max(by: {
+//            comparator($0[keyPath: keyPath], $1[keyPath: keyPath])
+//        })
+//    }
+//    
+//    func max<T: Comparable>(by keyPath: KeyPath<Element, T>) -> Element? {
+//        max(by: keyPath, >)
+//    }
+//}
